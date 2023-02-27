5d0b72233ccc97c60e4685e4b95c8c1a7b086840
wen <wisakura@outlook.com>
2023-02-28 00:37:34 +0800

    feat:监听文章目录，在开发环境中文件更改时重新构建数据

---
+++ generate.js
@@ -0,0 +1,48 @@
+const { compareDesc, format } = require('date-fns');
+const fs = require('fs-extra');
+const path = require('path');
+
+const matter = require('gray-matter');
+const postsDir = path.resolve(__dirname, 'posts');
+
+const getPostsPaths = async () => {
+  try {
+    const files = await fs.readdir(postsDir);
+    const paths = files.map((postPath) => [postPath, path.join(postsDir, postPath)]);
+    return paths;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const generate = async () => {
+  const paths = await getPostsPaths();
+  const result = [];
+  paths.forEach(([filename, path]) => {
+    const markdown = fs.readFileSync(path, 'utf-8');
+    const meta = matter(markdown);
+    meta.data.fmtDate = format(meta.data.date, 'yyyy-MM-dd');
+    meta.data.key = filename.replace('.mdx', '');
+    result.push(meta.data);
+  });
+  const sortResult = result.sort((a, b) => compareDesc(a.date, b.date));
+  const fmt = {
+    list: sortResult,
+  };
+  fs.writeFileSync(path.join(__dirname, 'src/data/data.json'), JSON.stringify(fmt), { encoding: 'utf-8' });
+};
+
+generate();
+console.log('generate posts data');
+
+const isDev = process.env.NODE_ENV === 'development';
+if (isDev) {
+  let timer = null;
+  fs.watch(postsDir, {}, () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      console.log('posts dir change!');
+      generate();
+    }, 50);
+  });
+}
--- next.config.mjs
+++ next.config.mjs
@@ -2,25 +2,26 @@ import nextMdx from '@next/mdx';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 import remarkGfm from 'remark-gfm';
+
 const withMDX = nextMdx({
-	extension: /\.mdx?$/,
-	options: {
-		jsx: true,
-		remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMdxFrontmatter],
-		rehypePlugins: [],
-	},
+  extension: /\.mdx?$/,
+  options: {
+    jsx: true,
+    remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMdxFrontmatter],
+    rehypePlugins: [],
+  },
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-	experimental: {
-		appDir: true,
-		mdxRs: true,
-		typedRoutes: true,
-	},
+  reactStrictMode: true,
+  swcMinify: true,
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  experimental: {
+    appDir: true,
+    mdxRs: true,
+    typedRoutes: true,
+  },
 };
 
 export default withMDX(nextConfig);
